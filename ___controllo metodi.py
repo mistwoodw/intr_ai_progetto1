0ce309c0a9fc694f5b62285690b892e648a4c6a7
Andrea Ceresa <andrea@Mac-di-Andrea.local>
2025-10-05 23:29:51 +0200

    Rimosso file

--- ___controllo metodi.py
+++
@@ -1,9 +0,0 @@
-import pysmile
-import pysmile_license
-
-net = pysmile.Network()
-ti = pysmile.TemporalInfo()
-
-# Mostra tutti i metodi e attributi disponibili
-#help(net.unroll())
-print(dir(ti))
